7c4e7d3eb6fc3eff584e40076f8dd6faf3e80312
pasthortown <pastorchessmaster@gmail.com>
2018-07-17 15:30:17 -0500

    ok

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
             { path: 'empresas', loadChildren: './empresas/empresas.module#EmpresasModule' },
             { path: 'hoja-vida', loadChildren: './hoja-vida/hoja-vida.module#HojaVidaModule' },
             { path: 'postulaciones', loadChildren: './postulaciones/postulaciones.module#PostulacionesModule' },
-            { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilModule' }
         ]
     }
 ];
