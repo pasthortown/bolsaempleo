c2293684e15444fa2889869bfbe968462a0fe2dd
pasthortown <pastorchessmaster@gmail.com>
2018-07-18 15:50:57 -0500

    ok

--- src/app/layout/postulaciones/postulaciones.component.ts
+++ src/app/layout/postulaciones/postulaciones.component.ts
@@ -11,5 +11,4 @@ export class PostulacionesComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
--- src/app/layout/postulantes/postulantes.component.ts
+++ src/app/layout/postulantes/postulantes.component.ts
@@ -1,3 +1,4 @@
+import { FirebaseBDDService } from './../../services/firebase-bdd.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
@@ -7,11 +8,26 @@ import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./postulantes.component.css']
 })
 export class PostulantesComponent implements OnInit {
+  contadorPersonas: number;
+  contadorEmpresas: number;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private firebaseBDDService: FirebaseBDDService) { }
 
   ngOnInit() {
+    this.contadorPersonas = 0;
+    this.contadorEmpresas = 0;
 
+    this.firebaseBDDService.firebaseControllerPostulantes.leer().snapshotChanges().subscribe(items => {
+      items.forEach(element => {
+        this.contadorPersonas++;
+      });
+    });
+
+    this.firebaseBDDService.firebaseControllerEmpresas.leer().snapshotChanges().subscribe(items => {
+      items.forEach(element => {
+        this.contadorEmpresas++;
+      });
+    });
   }
 
   open(content) {
