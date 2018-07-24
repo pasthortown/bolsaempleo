6c9c4b996121ac17674aa442b77101add4cf1822
elmachoalfa <gestrella@yavirac.edu.ec>
2018-07-24 09:24:58 -0500

    servicio global al fin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthService]
 })
 export class AppComponent {
   title = 'app';
+  constructor(private authService: AuthService) { }
 }
